a297e6a0bfb9e36fa1cd01a0b612698d22466e29
Hafis Babayev <riderblade1994@gmail.com>
2026-01-30 02:16:22 +0400

    Add zod

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,7 +1,66 @@
+"use client";
+
 import Link from "next/link";
 import { Input } from "./Input";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { ContactFormData, ErrorState } from "@/schemas/contact";
+import { z } from "zod";
 
-export async function RegisterForm() {
+export function RegisterForm() {
+  const [formData, setFormData] = useState<ContactFormData>({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState<ErrorState>({
+    username: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+    general: null,
+  });
+
+  const registerSchema = z
+    .object({
+      username: z.string().min(3, "Username must be at least 3 characters"),
+      email: z.email("Invalid email address"),
+      password: z.string().min(4, "Password must be at least 4 characters"),
+      confirmPassword: z.string(),
+    })
+    .refine((formData) => formData.password === formData.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: null });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const result = registerSchema.safeParse(formData);
+
+    if (!result.success) {
+      const flattened = z.flattenError(result.error);
+      const fieldErrors = flattened.fieldErrors; // не до конца понял что это делает
+
+      setErrors({
+        username: fieldErrors.username?.[0] ?? null,
+        email: fieldErrors.email?.[0] ?? null,
+        password: fieldErrors.password?.[0] ?? null,
+        confirmPassword: fieldErrors.confirmPassword?.[0] ?? null,
+        general: null,
+      });
+      return;
+    }
+  };
+  try {
+  } catch (err) {
+    console.log(err);
+  }
   return (
     <div className="grid xl:grid-cols-2 h-screen">
       <div className="bg-primary/80"></div>
@@ -12,20 +71,39 @@ export async function RegisterForm() {
         <p className="text-gray-400 text-lg font-medium">
           Enter personal data to create your account
         </p>
-        <form className="grid grid-cols-2 gap-6 sm:w-150 w-[90%] mt-8">
+        <form
+          onSubmit={handleSubmit}
+          className="grid grid-cols-2 gap-6 sm:w-150 w-[90%] mt-8"
+        >
           <label
             className="flex text-left col-span-2 sm:col-span-1 flex-col gap-2"
             htmlFor="username"
           >
             <span className="font-semibold">Username</span>
-            <Input type="text" id="username" placeholder="Enter username" />
+            <Input
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              type="text"
+              id="username"
+              placeholder="Enter username"
+              error={errors.username}
+            />
           </label>
           <label
             className="flex text-left col-span-2 sm:col-span-1 flex-col gap-2"
             htmlFor="email"
           >
             <span className="font-semibold">Email</span>
-            <Input type="email" id="email" placeholder="Enter email address" />
+            <Input
+              value={formData.email}
+              onChange={handleChange}
+              name="email"
+              type="email"
+              id="email"
+              placeholder="Enter email address"
+              error={errors.email}
+            />
           </label>
           <label
             className="flex text-left flex-col gap-2 col-span-2"
@@ -33,9 +111,13 @@ export async function RegisterForm() {
           >
             <span className="font-semibold">Password</span>
             <Input
+              value={formData.password}
+              onChange={handleChange}
+              name="password"
               type="password"
               id="password"
               placeholder="Enter your password"
+              error={errors.password}
             />
           </label>
           <label
@@ -44,9 +126,13 @@ export async function RegisterForm() {
           >
             <span className="font-semibold">Confirm password</span>
             <Input
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              name="confirmPassword"
               type="password"
               id="confirmPassword"
               placeholder="Confirm your password"
+              error={errors.confirmPassword}
             />
           </label>
           <button className="rounded-2xl bg-primary border border-transparent xl:hover:border-primary xl:hover:bg-transparent xl:hover:text-primary transition-all duration-300 p-3 col-span-2 font-medium cursor-pointer">
